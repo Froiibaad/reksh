bfa5e95b58ba4b214eb81cd9244f8255c49a64ab
hercegyu <hercegyu@4dd8ccb8-185c-11de-989c-57ac4e9a12f8>
2009-09-23 21:35:54 +0000

    Poceo da sklapam simulator..nema veze sto ima gresaka

---
+++ src/simulator/elements/BusIn16.java
@@ -0,0 +1,31 @@
+package simulator.elements;
+
+public class BusIn16 extends KombMreza {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BusIn16 (String name){
+		super(17,16); // in0..in15, signal, out0..out15
+		this.name = name;
+		for (int i=0; i<16; i++){
+			outputs.get(i).setHighZ();
+		}
+	}
+	protected void calc() {
+		if (inputs.get(16).getValue() == 1){
+			for (int i=0; i<16; i++){
+				outputs.get(i).set(inputs.get(i).getValue());
+			}
+		}
+		else {
+			for (int i=0; i<16; i++){
+				outputs.get(i).setHighZ();
+			}
+		}
+
+	}
+}
