bb39a2695c6283ca98bde5376bc2ef7449baa3e3
hercegyu <hercegyu@4dd8ccb8-185c-11de-989c-57ac4e9a12f8>
2009-09-28 16:57:36 +0000

    Oper2 duplirane linije

--- src/simulator/panels/PanelOper2.java
+++ src/simulator/panels/PanelOper2.java
@@ -16,57 +16,133 @@ public class PanelOper2 extends ImagePanel {
         g.drawImage(image, 0, 0, null);
         g.setColor(Color.GREEN);
         //IVTPin
+       //g.setColor(findColor(init.adder, 8));
         g.drawLine(499, 53, 506, 53);
+        g.drawLine(499, 54, 506, 54);
         //PCin
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(499, 114, 506, 114);
+        g.drawLine(499, 115, 506, 115);
         //IR1in
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(499, 171, 506, 171);
+        g.drawLine(499, 172, 506, 172);
         //IR2in
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(499, 228, 506, 228);
+        g.drawLine(499, 229, 506, 229);
         //IR3in
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(499, 286, 506, 286);
+        g.drawLine(499, 287, 506, 287);
         //IR4in
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(499, 343, 506, 343);
+        g.drawLine(499, 344, 506, 344);
         //MBRin
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(499, 402, 506, 402);
+        g.drawLine(499, 403, 506, 403);
         //MARmp
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(509, 492, 519, 492);
+        g.drawLine(509, 493, 519, 493);
         //MARin
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(499, 532, 506, 532);
+        g.drawLine(499, 533, 506, 533);
         //TEMP2mp
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(509, 601, 519, 601);
+        g.drawLine(509, 602, 519, 602);
         //IRRin
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(629, 729, 636, 729);
+        g.drawLine(629, 730, 636, 730);
         //IVTPM2out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(337, 68, 337, 75);
+        g.drawLine(336, 68, 336, 75);
         //IVTPM3out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(337, 97, 337, 104);
+        g.drawLine(336, 97, 336, 104);
         //PCM2out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(337, 124, 337, 131);
+        g.drawLine(336, 124, 336, 131);
         //PCM3out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(337, 152, 337, 159);
+        g.drawLine(336, 152, 336, 159);
         //IR1M2out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(337, 181, 337, 188);
+        g.drawLine(336, 181, 336, 188);
         //IR1M3out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(337, 209, 337, 217);
+        g.drawLine(336, 209, 336, 217);
         //IR2M2out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(337, 239, 337, 246);
+        g.drawLine(336, 239, 336, 246);
         //IR2M3out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(337, 267, 337, 273);
+        g.drawLine(336, 267, 336, 273);
+      //IR3M2out
+        //g.setColor(findColor(init.adder, 8));
+          g.drawLine(337, 296, 337, 305);
+          g.drawLine(336, 296, 336, 305);
+          //IR3M3out
+        //g.setColor(findColor(init.adder, 8));
+          g.drawLine(337, 324, 337, 333);
+          g.drawLine(336, 324, 336, 333);
         //IR4M2out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(337, 353, 337, 360);
+        g.drawLine(336, 353, 336, 360);
         //IR4M3out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(337, 382, 337, 389);
+        g.drawLine(336, 382, 336, 389);
         //MBRM2out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(337, 412, 337, 419);
+        g.drawLine(336, 412, 336, 419);
         //MBRM3out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(337, 440, 337, 447);
+        g.drawLine(336, 440, 336, 447);
         //MBRM1out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(704, 432, 704, 440);
+        g.drawLine(705, 432, 705, 440);
         //MARM1out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(704, 559, 704, 567);
+        g.drawLine(705, 559, 705, 567);
         //TEMP2M1out
+      //g.setColor(findColor(init.adder, 8));
         g.drawLine(705, 640, 705, 648);
+        g.drawLine(706, 640, 706, 648);
+        //TEMP2inLOW
+      //g.setColor(findColor(init.adder, 8));
+        g.drawLine(497, 634, 506, 634);
+        g.drawLine(497, 635, 506, 635);
+   //TEMP2swap
+      //g.setColor(findColor(init.adder, 8));
+        g.drawLine(497, 660, 506, 660);
+        g.drawLine(497, 661, 506, 661);
+     //TEMPinLOW
+    //g.setColor(findColor(init.adder, 8));
+        g.drawLine(628, 679, 637, 679);
+        g.drawLine(628, 680, 637, 680);
+      //TEMPswap
+      //g.setColor(findColor(init.adder, 8));
+        g.drawLine(628, 701, 637, 701);
+        g.drawLine(628, 702, 637, 702);     
         
        //TEMP2inLOW
         //g.drawLine(499, 634, 506, 634);
