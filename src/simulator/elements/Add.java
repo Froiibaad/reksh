02812280e9a88641af943e48249c1caed6c8f9ef
sve.cool.je.vec.zauzeto <sve.cool.je.vec.zauzeto@4dd8ccb8-185c-11de-989c-57ac4e9a12f8>
2009-09-28 10:01:31 +0000

    Kometarisano dodavanje addera za ax+bx i promenjen adder

--- src/simulator/elements/Add.java
+++ src/simulator/elements/Add.java
@@ -1,18 +1,18 @@
 package simulator.elements;
 
 public class Add extends KombMreza {
+	//public static int C0 = 32;
     public Add() {
-        super(17, 9); // A0-A7, B0-B7, C0 // OUT0-OUT7, C8
+        super(33, 17); // A0-A15, B0-B15, C0 // OUT0-OUT15, C8
     }
 
     protected void calc() {
-        int c = inputs.get(16).getValue();
-        for (int i = 0; i < 8; i++) {
-            int add = inputs.get(i).getValue() + inputs.get(i + 8).getValue() +
-                      c;
+        int c = inputs.get(32).getValue();
+        for (int i = 0; i < 16; i++) {
+            int add = inputs.get(i).getValue() + inputs.get(i + 16).getValue() + c;
             c = add / 2;
             outputs.get(i).set(add % 2);
         }
-        outputs.get(8).set(c);
+        outputs.get(16).set(c);
     }
 }
